94273d0d853e54669fceeffbc258087993347c0b
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-10 18:38:05 +0800

    Day3.java

--- Day3.java
+++ Day3.java
@@ -1,22 +1,16 @@
-package day3;
+package dayyy3;
 
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Haitasya");
-        //hai.ini penggunaan komentar satu baris 
-        
-        
-        System.out.print("gmnrsnyajdimahasiswa?");
-        /* gatau,bingung,yg psti bisa bikin stress
-         * dan blablablalablablablablabla
-         * blablablablablablablablabla bla
-         * blablabla blablablablabla blabla.
-         * komentar ini dipergunakan untuk
-         * penjelasan yang panjang dan bisa
-         * multi baris
+        System.out.println("say hi!");
+        //ini penggunaan komentar satu baris
+         
+        System.out.println("hi");
+        /* Komentar ini dipergunakan untuk 
+         * komentar yang panjang ataupun multi
+         * baris 
          */
-        
     }
     
 }
