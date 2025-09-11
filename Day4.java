16593e3d440fa437f9acfafff89332b1598ae1f2
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-11 19:18:54 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,31 @@
+package day4;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("penggunaan escape sequence");
+        
+        System.out.println("hai\nhai jg");
+        // \n : membuat teks pindah ke baris berikurnya
+        
+        System.out.println("nama:\tsya");
+        // \t : membuat spasi
+        
+        System.out.println("aku:aku\\dia?");
+        // \\ : membuat satu blackslas 
+        
+        System.out.println("Dia menjawab:\"dia");
+        // \" : membuat tanda kutip ganda
+        
+        System.out.println("oh oke\b");
+        // \b : membuat 1 karakter terhapus sebelum \b
+        
+        System.out.println("it\'s");
+        // \' : membuat tanda kutip tunggal
+        
+        System.out.println("oasya\rT");
+        // \r : membuat kursol kembali ke awal baris[o diganti T]
+        
+    }
+    
+}
