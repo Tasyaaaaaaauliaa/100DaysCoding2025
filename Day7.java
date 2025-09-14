7d32aaeb3adfbd2afb549e58ea8dce4ea213b718
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-14 16:32:00 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,28 @@
+package day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Tipe Data Karakter (char) dan Boolean (boolean)");
+        
+        char huruf = ('s');
+        char angka = ('5');
+        char simbol = ('$');
+        System.out.println(huruf);
+        System.out.println(angka);
+        System.out.println(simbol);
+        /*char digunakan untuk satu karakter tunggal
+         * seperti huruf,angka dan simbol
+         */
+         
+        boolean tasya = true;
+        boolean dasya = false;
+        System.out.println(tasya);
+        System.out.println(dasya);
+        /*boolean hanya memiliki 2 nikai yaitu
+         * true(benar) dan false(salah)
+         */
+        
+    }
+    
+}
