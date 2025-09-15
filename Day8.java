4d850c62f7b00e31f897a78a5294e453b3f01dd4
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-15 20:56:24 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,17 @@
+package day8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Pengenalan Tipe Data String untuk teks");
+        
+        String kata = "aku";
+        String keinginan = "tidur";
+        System.out.println(kata);
+        System.out.println("mau," + keinginan);
+        /*String dipergunakan untuk teks dan ini tipe
+         * data non primitif dan mengguanakn tanda kutip("")
+         */
+    }
+    
+}
