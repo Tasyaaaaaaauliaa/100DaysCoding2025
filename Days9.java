48644346fb350a7d9d9eb3a43becb97faae9d6ae
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-16 12:06:34 +0800

    Days9.java

---
+++ Days9.java
@@ -0,0 +1,16 @@
+package ejeh;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        int nilaiawal = 20000;
+        //System.out.println("nilai awal," + nilaiawal);
+        
+        nilaiawal = 15000;
+        System.out.println("nilai update," + nilaiawal);
+        
+        
+    }
+    
+}
