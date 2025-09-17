5d694d59c7003e0be0a587581393508e478970f5
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-17 20:16:54 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,16 @@
+package day10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        final int MAX_NUMBER = 2;
+        //MAX_NUMBER = 5;××> akan eror karna variable sudah final
+        System.out.println("maksimal nomor:"+ MAX_NUMBER);
+        /* jadi,konstanta adalah variabel yang nilainya tidak
+         * bisa diubah setelah diinisialisasi.
+         * Konstanta dibuat dengan menggunakan keyword final.
+         */
+        
+    }
+    
+}
