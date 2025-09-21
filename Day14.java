7a7b3410e74150c9f2b2ad1f05a2f90dca401776
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-21 17:21:26 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,23 @@
+package day14;
+
+public class Main {
+    
+    public static void main(String[] args) {
+      String umur = "19";
+        int ror = Integer.parseInt(umur);
+        System.out.println(umur);
+        
+        String tinggi = "151.2";
+        double dor = Double.parseDouble(tinggi);
+        System.out.println(tinggi);
+        
+        String ang = "20";
+        byte kor = Byte.parseByte(ang);
+        System.out.println(ang);
+        
+        String ka = "30";
+        long dos = Long.parseLong(ka);
+        System.out.println(dos);
+    }
+    
+}
