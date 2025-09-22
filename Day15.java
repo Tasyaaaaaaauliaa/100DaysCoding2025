4c22be3e4feb048d62d096814777349fd71c2138
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-22 21:08:40 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+package day15;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 15;
+        int penjumlahan = a + b;
+        int pengurangan = a - b;
+        
+        System.out.print("Hasil Penjumlahan:"+ penjumlahan);
+        System.out.print("Hasil Pengurangan:"+ pengurangan);
+    }
+    
+}
