5bb2c37e1e4c7b64d1b67904b87331553e390187
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-23 20:29:36 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,18 @@
+package Day15;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        double t = 18;
+        double s = 5;
+         
+        double perkalian = t * s;
+        double pembagian = t / s;
+        
+        System.out.print("Hasil Perkalian:"+ perkalian);
+        System.out.print("Hasil Pembagian:"+ pembagian);
+    
+    }
+    
+}
