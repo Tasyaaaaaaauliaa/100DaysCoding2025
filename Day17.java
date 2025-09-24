14516bccf9b2e3fc976205f9ce5c21d2c4e8cb71
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-24 19:19:27 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,15 @@
+package day17;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        // % : modulus atau sisa bagi
+        int p = 45;
+        int y = 7;
+        
+        int hasil = p % y;
+        System.out.print("Hasil sisa bagi :"+ hasil);
+        
+    }
+    
+}
