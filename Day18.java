8302f87073f11630f6a2a8e700f9eac5a0c24ef1
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-25 20:22:59 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package ty;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        long angka = 45;
+        System.out.println("Long = "+ angka);
+        double angka1 = (double)+ angka;
+        System.out.println("Double="+ angka1);
+        
+    }
+    
+}
