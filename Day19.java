3e366bd1f6a24713eedfa99ad8b653329247786f
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-09-26 21:29:56 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,13 @@
+package Day19;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil)
+        double angka1 = 12.2;
+        System.out.println("Double = "+ angka1);
+        int angka2 = (int) angka1;
+        System.out.println("Int =" + angka2);
+    }
+    
+}
