73012c0d9b5c5963c3ddc7c29a45c104bfe8decc
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-02 18:57:44 +0800

    Day25.java

--- Day25.java
+++ Day25.java
@@ -2,13 +2,13 @@ package Day25;
 
 public class Day25 {
     public static void main(String[] args) {
-        
+      //incremen:++
     byte a = 5;
     a++;
     System.out.println(a);
-    
+    //decremen:--
     int b = 7;
-    b++;
+    b--;
     System.out.print(b);
     
 }
