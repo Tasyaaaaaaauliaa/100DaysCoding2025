2fa8c3fe9be355a704a83e39da6ffb8acdc768f3
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-02 18:52:10 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,16 @@
+package Day25;
+
+public class Day25 {
+    public static void main(String[] args) {
+        
+    byte a = 5;
+    a++;
+    System.out.println(a);
+    
+    int b = 7;
+    b++;
+    System.out.print(b);
+    
+}
+    
+}
