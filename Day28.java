5e8a4c4b2e6ad27c5f204c9a4ad1c52e085cca6f
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-05 18:03:25 +0800

    Day28.java

---
+++ Day28.java
@@ -0,0 +1,12 @@
+package Day28;
+import java.util.Scanner;
+public class day28 {
+    public static void main(String[]Argd){
+        Scanner x = new Scanner(System.in);
+        int a = x.nextInt();
+        int b = x.nextInt();
+        
+        System.out.println(a == b);
+        System.out.println(a != b);
+    }
+}
