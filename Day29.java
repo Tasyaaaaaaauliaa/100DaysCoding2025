8ceca00567bfcd5649a39fe736bb0453a717b442
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-06 20:25:41 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,11 @@
+package Day29;
+import java.util.Scanner;
+public class day29 {
+    public static void main(String[]Argd){
+        Scanner x = new Scanner(System.in);
+        byte a = x.nextByte();
+        long b = x.nextLong();
+        System.out.println(a < b);
+        System.out.println(a > b);
+    }
+}
