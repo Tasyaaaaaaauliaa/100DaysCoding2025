4582dc4b8b156813fb960e5d1d897e32ad0c1fb3
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-07 20:17:01 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,16 @@
+package day30;
+import java.util.Scanner;
+public class day30 {
+    public static void main(String[]Argd){
+        Scanner x = new Scanner (System.in);
+        
+        int a = x.nextInt();
+        int b = x.nextInt();
+
+        System.out.println("Hasil Perbandingan:");
+        System.out.println(a + " <= " + b + " : " + (a <= b));
+        System.out.println(a + " >= " + b + " : " + (a >= b));
+    
+
+    }
+    }
