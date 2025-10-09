1330c95ccbd78982228f898136039413a0149a39
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-09 21:00:46 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,19 @@
+package day32;
+import java.util.Scanner;
+public class dayy32 {
+    public static void main(String[] args) {
+        Scanner x = new Scanner (System.in);
+        System.out.print("Masukan nilai a : ");
+        int a = x.nextInt();
+        System.out.print("Masukan nilai b : ");
+        int b = x.nextInt();
+        System.out.print("Masukan nilai c : ");
+        int c = x.nextInt();
+        System.out.print("hasil operator OR : ");
+        System.out.print(a > b || a < c);
+        
+        
+        
+    }
+    
+}
