1603a9ca544f4f7bdd627da1ff97123b6d3b6f8b
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-29 20:51:52 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+package Day52;
+public class day52 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) { 
+            System.out.println("Angka ke-" + i);
+            i++;   
+        }
+    }
+}
+
