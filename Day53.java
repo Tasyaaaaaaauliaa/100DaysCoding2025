497caf72b38e9c777d3557e833c1516f847b5bca
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-30 21:21:58 +0800

    Day53.java

---
+++ Day53.java
@@ -0,0 +1,12 @@
+package day54;
+public class day53 {
+    public static void main(String[]args) {
+       
+        for (int i = 0; i < 20; i++) {
+            if (i == 9 ) {
+                break;
+            }
+           System.out.println(i );
+        }
+    }
+}
