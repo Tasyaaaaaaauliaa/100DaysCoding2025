d178248347f78d4afdd0a3c64178c0b96eb95906
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-10-31 20:23:15 +0800

    Day54.java

--- Day54.java
+++ Day54.java
@@ -3,7 +3,7 @@ public class dayy54 {
     public static void main(String[]args) {
       for (int i = 1; i <= 10; i++) {
             if (i == 5) {
-                continue; // Lewati iterasi saat i bernilai 5
+                continue;
             }
             System.out.println("Nilai i: " + i);
         }
