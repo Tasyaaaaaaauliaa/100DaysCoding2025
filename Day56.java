d6c81adf7cbb0538516449591bdfb32ea64e316e
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-02 21:41:39 +0800

    Day56.java

--- Day56.java
+++ Day56.java
@@ -2,14 +2,14 @@ package day56;
 import java.util.Scanner;
 public class day56 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner x = new Scanner(System.in);
         int angka;
 
         do {
-            System.out.print("masukan angka (1-5) agar cocok: ");
-            angka = scanner.nextInt();
+            System.out.print("masukan angka (1-5) : ");
+            angka = x.nextInt();
         } while (angka < 1 || angka > 5);
 
-        System.out.println("angka: " + angka + ",ini cocok");
+        System.out.println("angka: " + angka );
     }
 }
