6fabea9e4b957487422227c84d7292ffb1eb0524
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-11 20:17:10 +0800

    Day65.java

---
+++ Day65.java
@@ -0,0 +1,18 @@
+package day65;
+import java.util.Scanner;
+public class day65 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        long faktorial = 1;
+        for (int i = 1; i <= n; i++) {
+            faktorial *= i;
+        }
+
+        System.out.println( "nilai dari "+ n + "! adalah: " + faktorial);
+    }
+}
