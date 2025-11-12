5a3ab3645dfb014c2cfdcb392126eba4c56d7198
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-12 23:47:36 +0800

    Day66.java

--- Day66.java
+++ Day66.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class day66 {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        System.out.print("tnggi pola: ");
+        System.out.println("tinggi pola");
         int tp = x.nextInt();
 
         for (int i = 1; i <= tp; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("* ");
             }
              System.out.println();
         }
