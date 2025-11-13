9e9fd1e3d965907067b08121e4ea1c368e084822
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-13 21:07:36 +0800

    Day67.java

---
+++ Day67.java
@@ -0,0 +1,16 @@
+package day67;
+import java.util.Scanner;
+public class day67{
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.println("lebar pola");
+        int lp = x.nextInt();
+
+        for (int i = 1; i <= lp; i++) {
+                System.out.print(" * ");
+            
+        }
+    }
+
+}
+
