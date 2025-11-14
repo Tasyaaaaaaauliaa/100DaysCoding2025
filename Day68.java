5f72a2387d93d722f46ad776929af5c43a5b897b
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-14 20:22:14 +0800

    Day68.java

---
+++ Day68.java
@@ -0,0 +1,15 @@
+package day68;
+import java.util.Scanner;
+public class day68 {
+    public static void main(String[] args) {
+    Scanner x = new Scanner(System.in);
+        System.out.println("input:");
+        int a = x.nextInt();
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1;j <= a ;j++){
+                System.out.print("*");
+            } 
+            System.out.println();
+}
+}
+}
