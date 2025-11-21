52572b44ac8b60b631d93215ffa8e9b24b769fc5
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-21 20:36:48 +0800

    Day75.java

---
+++ Day75.java
@@ -0,0 +1,13 @@
+public class day75 {
+    
+    public static void main(String[] args) {
+        String hai = "hai syya";
+        
+        System.out.println("Panjang string: " + hai.length());
+        System.out.println("huruf besar: " + hai.toUpperCase());
+        System.out.println("huruf kecil: " + hai.toLowerCase());
+    
+        
+         }
+    
+}
