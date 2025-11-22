b83039f7ed8a04ab0d562fed300f9a7f9ab9cbab
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-22 20:42:09 +0800

    Day76.java

---
+++ Day76.java
@@ -0,0 +1,17 @@
+public class day76 {
+      public static void main(String[] args) {
+        String nama = "Tasya";
+        String name = "syyi";
+        String nome = "";
+          
+        System.out.println(nama.equals(nama)); 
+          
+        System.out.println(name.equalsIgnoreCase(nome));
+          
+        System.out.println(name.contains("sy"));
+          
+        System.out.println(name.isEmpty()); 
+        System.out.println(nome.isEmpty()); 
+    }
+
+}
