e161ed89962182b7368bcab97cfadfdec691a023
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-24 21:28:32 +0800

    Day78.java

--- Day78.java
+++ Day78.java
@@ -6,9 +6,9 @@ public class day78 {
         System.out.println("Huruf besar: " + kata.toUpperCase());
         System.out.println("Huruf kecil: " + kata.toLowerCase());
         System.out.println("Substring : " + kata.substring(8));
-        System.out.println("String mengambil kata pertama  : " + kata.replace("tasya", "auliaa"));
+        System.out.println("String mengganti kata   : " + kata.replace("tasya", "auliaa"));
         System.out.println("String tanpa spasi di awal dan akhir: " + kata.trim());
-        System.out.println("Apakah string mengandung " + kata.contains("nama "));
+        System.out.println("Apakah string mengandung 'nama'? " + kata.contains("nama "));
         System.out.println("String kosong : " + kata.isEmpty());
     }
 }
