917cc6441a41005adc6b2f5dcecbab23837b385a
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-11-27 21:51:00 +0800

    Day81.java

--- Day81.java
+++ Day81.java
@@ -2,19 +2,12 @@ import java.util.Scanner;
 public class day81 {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        int[] array = {10, 20, 30, 40, 50};
+        int[] array = {28, 8, 20, 6, 20};
 
-        System.out.println("Isi array:");
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("Indeks ke-" + i + ":" + array[i]);
-        }
-        System.out.print("indeks yang diakses: ");
+        System.out.println("Indeks ke :");
         int indeks = x.nextInt();
+        System.out.println("isi indeks ke "+ indeks + " adalah " + array[indeks]);
         
-        if (indeks >= 0 && indeks < array.length) {
-            System.out.println("indeks ke-" + indeks + " adalah: " + array[indeks]);
-        } else {
-            System.out.println("Indeks tidak valid!");
-        }
+        
     }
 }
