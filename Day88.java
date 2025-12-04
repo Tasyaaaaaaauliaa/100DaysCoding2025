89d5b2c8718b43ade2e60410e5a4b6ad5817e82e
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-12-04 17:45:10 +0800

    Day88.java

--- Day88.java
+++ Day88.java
@@ -1,8 +1,8 @@
 public class day88 {
     public static void main(String[] args) {
             String[] nama = {"tasya", "aulia", "caca", "aya"};
-    for (String namaa : nama) {
-        System.out.println(namaa);
+    for (String elemen : nama) {
+        System.out.println(elemen);
     }
     }
 }
