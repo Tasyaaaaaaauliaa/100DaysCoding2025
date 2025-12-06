02793752517a80dbcd5e7f91bb27add878830010
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-12-06 12:20:14 +0800

    Day90.java

---
+++ Day90.java
@@ -0,0 +1,10 @@
+public class day90 {
+      public static int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int hasil = tambah(1, 1);
+        System.out.println(hasil);
+    }
+}
