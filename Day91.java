a38e87981dff2fa1eee7e55621cea62f2dc303d9
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-12-07 20:48:58 +0800

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,9 @@
+public class day91 {
+    public static void ab(String z) {
+        System.out.print(z);
+    }  
+        public static void main(String[] args) {
+        ab("hi");
+            ab("ha");
+        }
+}
