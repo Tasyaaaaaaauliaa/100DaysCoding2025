eb60db8c2f9da850b10c15c1af32ac0d8ee419f1
Tasyaaaaaaauliaa <taysaauliatasyaaulia07@gmail.com>
2025-12-10 18:56:09 +0800

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class day94 {
+      public static void main(String[] args) {
+       Scanner x = new Scanner(System.in);
+       String a = x.nextLine();
+          mk(a);
+    }
+      public static void mk(String a) {
+    System.out.println("mata kuliah : " + a);
+    }
+}
